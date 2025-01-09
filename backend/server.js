8230fe7a2ac7596c786094f91cb01782ed265c61
vscodeliver <writer.paulo.coelho@gmail.com>
2025-01-09 22:13:46 +0300

    Added Vite preview URL to CORS origins

--- backend/server.js
+++ backend/server.js
@@ -26,6 +26,7 @@ app.use(
   cors({
     origin: [
       "http://localhost:5173",
+      "http://localhost:4173",
       "https://userauthapp-production.up.railway.app",
     ], // Укажите точный адрес фронтенда
     credentials: true, // Разрешить отправку cookies
