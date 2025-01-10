f5546a794864be69267506e3bfcd0e7579e0d639
vscodeliver <writer.paulo.coelho@gmail.com>
2025-01-10 12:38:00 +0300

    HTTP only cookies

--- backend/server.js
+++ backend/server.js
@@ -47,7 +47,7 @@ app.use(
     saveUninitialized: true,
     cookie: {
       secure: PROD_MODE,
-      httpOnly: false,
+      httpOnly: true,
       sameSite: "none",
     }, // Для разработки secure: false (HTTPS не требуется)
   })
