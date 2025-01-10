6c1331719c97e24da4f82a221cdb8b6c852d755a
vscodeliver <writer.paulo.coelho@gmail.com>
2025-01-10 19:50:45 +0300

    Returned static public folder on the backend

--- backend/server.js
+++ backend/server.js
@@ -114,9 +114,9 @@ app.use("/user", userRoutes);
 // app.use(express.static(path.join(__dirname, "public")));
 
 // Перенаправление всех запросов на фронтенд
-// app.get("*", (req, res) => {
-//   res.sendFile(path.join(__dirname, "public", "index.html"));
-// });
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "public", "index.html"));
+});
 
 app.listen(PORT, () => {
   console.log(`Сервер доступен по адресу http://localhost:${PORT}`);
